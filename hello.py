2df9df17471fc74b82eec9faf7290a53d7d79dda
Eduardo Júnior <eduardomsj@gmail.com>
2023-12-01 00:13:01 -0300

    Implemented multi language Hello World

--- hello.py
+++ hello.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python3
 
-print("Hello World!")
+"""Hello World Multi Language.
+
+Messages displayed accordingly to configured environmente language.
+
+Usage:
+
+LANG variable approppriately configured:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Eduardo Moura"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
+
+print(msg)
